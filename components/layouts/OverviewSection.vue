77e4cbf9632a3df9834a545f1fbe7326200f5a6b
imfarzdfrh <imfrzdf@gmail.com>
2026-02-17 19:18:06 +0330

    Fix UI inconsistencies on the homepage

--- components/layouts/OverviewSection.vue
+++ components/layouts/OverviewSection.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="py-16 bg-lightBackground dark:bg-darkBackground">
-      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-screen-xl mx-auto px-5 sm:px-6 lg:px-8">
         <div class="text-center mb-12">
           <h2 class="text-3xl font-extrabold text-darkText dark:text-lightText">
             Website and Service Overview
           </h2>
           <p class="mt-4 text-lg text-darkText dark:text-lightText">
-            Experience unique and professional use of AI tools and services with us.
+            Experience unique and professional use of AI tools and services with us
           </p>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 dark:bg-gray-800/50">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 dark:bg-gray-800/50">
           <div class=" dark:bg-darkElement p-6 rounded-lg shadow-lg">
             <div class="flex items-center justify-center mb-6">
               <img src="/assets/icons/gif/support.gif" class="w-16 h-16 m-5" />
             </div>
             <h3 class="text-2xl font-semibold text-darkText dark:text-lightText">Technical Support</h3>
             <p class="mt-3 dark:text-lightText">
-              We provide support throughout your journey, from setup to ongoing assistance.
+              We provide support throughout your journey, from setup to ongoing assistance
             </p>
           </div>
 
@@ -27,7 +27,7 @@
             </div>
             <h3 class="text-2xl font-semibold text-darkText dark:text-lightText">Fast Services</h3>
             <p class="mt-3 text-darkText dark:text-lightText">
-              Our services are designed for you to achieve your goals with speed and efficiency.
+              Our services are designed for you to achieve your goals with speed and efficiency
             </p>
           </div>
 
@@ -37,7 +37,7 @@
             </div>
             <h3 class="text-2xl font-semibold text-darkText dark:text-lightText">Large Community</h3>
             <p class="mt-3 text-darkText dark:text-lightText">
-              Join our network of experts and users to learn from each other's experiences.
+              Join our network of experts and users to learn from each other's experiences
             </p>
           </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
 
-const isContentLoaded = ref(false);
+const isContentLoaded = ref(false)
 
 onMounted(() => {
   setTimeout(() => {
-    isContentLoaded.value = true;
-  }, 100); 
-});
+    isContentLoaded.value = true
+  }, 100)
+})
 </script>
 
 <template>
   <div class="relative">
     <LayoutsMainBanner />
-    <LayoutsOverviewSection />
+
     <LayoutsMainProduct />
-    <LayoutsCategory />
     <LayoutsListProduct />
+    <LayoutsCategory />
+    <LayoutsOverviewSection />
     <LayoutsFooter v-if="isContentLoaded" />
   </div>
 </template>

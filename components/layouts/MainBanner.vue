3311f46fa22f1d1f02ec551bed419fd8831cc0f1
imfarzdfrh <imfrzdf@gmail.com>
2026-02-21 21:56:48 +0330

    Link Button Explorer to ProductPage

--- components/layouts/MainBanner.vue
+++ components/layouts/MainBanner.vue
@@ -7,7 +7,7 @@
     <h1 class="mb-6 text-8xl font-bold">Ai Product Best</h1>
     <p class="text-lg">Lorem ipsum available build crypto compo is different website</p>
     <p class="mb-8 text-lg">AI Chat is an AI chatbot that writes text. You can use it to write stories, messages, or programming code</p>
-    <UiBaseButton color="primary" size="large">Explorer</UiBaseButton>
+    <UiBaseButton @click="$router.push('/ProductPage')" color="primary" size="large">Explorer</UiBaseButton>
 
     <div class="flex justify-center items-center mt-10">
       <div class="absolute bottom-20 left-1/2 transform -translate-x-1/2">

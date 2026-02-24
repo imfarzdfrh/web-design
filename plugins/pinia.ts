adac41764837d6f5717de68642b0236092f3f7d9
imfarzdfrh <imfrzdf@gmail.com>
2026-02-24 23:19:04 +0330

    fix bug typescripts file

--- plugins/pinia.ts
+++ plugins/pinia.ts
@@ -1,8 +1,7 @@
-import { defineNuxtPlugin } from '#app'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
-})
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
--- stores/cart.ts
+++ stores/cart.ts
@@ -1,30 +1,18 @@
 // stores/cart.ts
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+export const useCartStore = defineStore('cart', {
+  state: () => ({
+    cartItems: []
+  }),
 
-export const useCartStore = defineStore('cart', () => {
-  const cartItems = ref<any[]>([])
-
-  function addToCart(product: any) {
-    const existing = cartItems.value.find(p => p.id === product.id)
-    if (existing) {
-      existing.quantity += 1
-    } else {
-      cartItems.value.push({ ...product, quantity: 1 })
+  actions: {
+    addToCart(product) {
+      this.cartItems.push(product)
+    },
+    removeFromCart(id) {
+      this.cartItems = this.cartItems.filter(item => item.id !== id)
     }
-  }
+  },
 
-  function removeFromCart(id: number) {
-    cartItems.value = cartItems.value.filter(item => item.id !== id)
-  }
-
-  return {
-    cartItems,
-    addToCart,
-    removeFromCart,
-  }
-},
-  {
-    persist: true
-  }
-)
+  persist: true // 👈 همین خط جادویی
+})

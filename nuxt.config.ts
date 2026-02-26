4df61e94292ee32335ee4986a23300af0f81d66c
imfarzdfrh <imfrzdf@gmail.com>
2026-02-26 22:19:07 +0330

    Add @pinia-plugin-persistedstate/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt','@pinia-plugin-persistedstate/nuxt'],
   css: ['@/assets/css/main.css']
 })

77add1e0e9766e0f9a70834911777af997cd54f9
imfarzdfrh <imfrzdf@gmail.com>
2026-02-26 22:19:20 +0330

    create component steps

---
+++ components/layouts/Steps.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center gap-10 my-24">
+    <div class="flex items-center gap-2">
+      <span
+        class="w-8 h-8 rounded-full bg-black dark:bg-gray-600/50 text-white flex items-center justify-center"
+        >1</span
+      >
+      <span class="font-medium">Shopping cart</span>
+    </div>
+    <div class="flex items-center gap-2 text-gray-400">
+      <span
+        class="w-8 h-8 rounded-full bg-gray-200 dark:text-darkText flex items-center justify-center"
+        >2</span
+      >
+      <span>Checkout details</span>
+    </div>
+    <div class="flex items-center gap-2 text-gray-400">
+      <span
+        class="w-8 h-8 rounded-full bg-gray-200 dark:text-darkText flex items-center justify-center"
+        >3</span
+      >
+      <span>Order complete</span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
